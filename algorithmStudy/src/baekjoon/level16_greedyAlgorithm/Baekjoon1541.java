da26570f48508779f944af2f141d4abc31d9c533
dpsk331 <dpsk331@naver.com>
2021-10-29 11:31:53 +0900

    Baekjoon1541 잃어버린 괄호
    문제 풀이 성공!

--- algorithmStudy/src/baekjoon/level16_greedyAlgorithm/Baekjoon1541.java
+++ algorithmStudy/src/baekjoon/level16_greedyAlgorithm/Baekjoon1541.java
@@ -7,16 +7,24 @@ import java.util.Scanner;
 public class Baekjoon1541 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int result = 0;
+        String[] arr = sc.nextLine().split("-");
 
-        String[] arr = str.split("-|\\+");
+        int result = Integer.MAX_VALUE; //Integer.MAX_VALUE로 줘야 함!
 
         for(int i=0; i<arr.length; i++) {
-            if(i == 0) {
-                result += Integer.parseInt(arr[i]);
+            int temp = 0;
+
+            //"-"으로 나뉜 토큰을 "+"로 나누거나 +가 포함되지 않을 경우 현재 값 그대로 가져옴
+            String[] plusArr = arr[i].split("\\+");
+
+            for(String s : plusArr) {
+                temp += Integer.parseInt(s);
+            }
+
+            if(result == Integer.MAX_VALUE) { //첫번째 수가 들어오는 경우
+                result = temp;
             } else {
-                result -= Integer.parseInt(arr[i]);
+                result -= temp;
             }
         }
 
