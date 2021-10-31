7925a793e436089b4a5836950eb00c6d4d5f2439
dpsk331 <dpsk331@naver.com>
2021-10-31 22:53:56 +0900

    Baekjoon2447 별 찍기

--- algorithmStudy/src/baekjoon/level10_recursion/Baekjoon2447.java
+++ algorithmStudy/src/baekjoon/level10_recursion/Baekjoon2447.java
@@ -1,24 +1,43 @@
 package baekjoon.level10_recursion;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 // 백준 2447 문제] 별 찍기 - 10
 public class Baekjoon2447 {
-    public static void star() {
+    static char[][] starArr;
 
+    public static void star(int x, int y, int N) {
+        if(N == 1) {
+            starArr[x][y] = '*';
+            return;
+        }
+
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                if(i == 1 && j == 1) {
+                    continue;
+                } else {
+                    star(x+((N/3)*i), y+((N/3)*j), (N/3));
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int ex = n/3;
+        int N = sc.nextInt();
 
-        char[][] star = new char[n][n];
+        starArr = new char[N][N];
 
-        // (1,1) 일 때 공백
+        for(int i=0;i<starArr.length;i++) {
+            Arrays.fill(starArr[i],' ');
+        }
 
+        star(0, 0, N);
 
-
-
+        for(int i=0;i<starArr.length;i++) {
+            System.out.println(starArr[i]);
+        }
     }
 }
